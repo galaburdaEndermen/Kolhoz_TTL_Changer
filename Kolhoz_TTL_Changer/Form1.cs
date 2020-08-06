a07bd5cbec2a08717560fc0ecd1a2ea2b919251c
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-08-06 19:20:46 +0300

    there is only registry-writing left

--- Kolhoz_TTL_Changer/Form1.cs
+++ Kolhoz_TTL_Changer/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,16 +18,62 @@ namespace Kolhoz_TTL_Changer
         public Form1()
         {
             InitializeComponent();
+
+            if (!IsAdministrator())
+            {
+                // Restart and run as admin
+                var exeName = Process.GetCurrentProcess().MainModule.FileName;
+                ProcessStartInfo startInfo = new ProcessStartInfo(exeName);
+                startInfo.Verb = "runas";
+                startInfo.Arguments = "restart";
+                Process.Start(startInfo);
+                //Application.Exit();
+
+                Process.GetCurrentProcess().Kill();
+                //this.Close();
+            }
         }
 
+        public static bool IsAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private int bits = 0;
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            if (Environment.Is64BitOperatingSystem)
+            {
+                bits = 64;
+            }
+            else
+            {
+                bits = 32;
+            }
+            this.Text = $"TTL_Changer {bits}-bit";
+            if (IsAdministrator())
+            {
+                this.Text += "(Administrator)";
+            }
         }
 
         private bool ChangeTTL(int value)
         {
-            MessageBox.Show(value.ToString());
+            switch (bits)
+            {
+                case 32:
+                    MessageBox.Show("32");
+                    break;
+                case 64:
+                    MessageBox.Show("64");
+                    break;
+                default:
+                    MessageBox.Show("System instruction set recognition error!");
+                    break;
+            }
+
             return false;
         }
 
