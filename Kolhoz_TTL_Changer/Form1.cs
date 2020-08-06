8696de3e795b05bffaab338ea0a59023c4b94dd2
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-08-06 17:50:52 +0300

    gui mostly implemented

--- Kolhoz_TTL_Changer/Form1.cs
+++ Kolhoz_TTL_Changer/Form1.cs
@@ -10,11 +10,44 @@ using System.Windows.Forms;
 
 namespace Kolhoz_TTL_Changer
 {
+
+    //TTL   Операционная система
+    //54	FreeBSD / BSD
+    //64	Linux
+    //128	Windows
+    //255	Cisco / Solaris
+    //65    Android
+    //65    IOS
+    //130   Lumia
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Change
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Reset
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Info
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
